4cb931d65e27c399cddca1406e183bc1b87d4f97
Marlin Cremers <marlinc@mms-projects.net>
2012-01-26 00:02:19 +0100

    The config template is now PEAR CS valid

--- src/config-template.php
+++ src/config-template.php
@@ -1,6 +1,14 @@
 <?php
 /**
  * This file was created automatically editing it might break things
+ * 
+ * PHP version 5
+ * 
+ * @category NPO-project
+ * @package  NPO-bot
+ * @author   Marlin Cremers <marlinc@mms-projects.net>
+ * @license  http://creativecommons.org/licenses/by-nc/3.0/ Creative Commons Attribution-NonCommercial 3.0 Unported License
+ * @link     https://github.com/NPO-project/NPO-bot
  */
 
 $config = array(
